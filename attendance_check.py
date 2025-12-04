586cc568a8ac88f4ecf2248be5b0d31b89beb07e
Venomsec45 <dayshaunmarnin@gmail.com>
2025-12-04 22:32:33 +0800

    Added search status feature

--- attendance_check.py
+++ attendance_check.py
@@ -6,6 +6,7 @@ students = []
 present_students = []
 absent_students = []
 
+# Enroll students
 def enroll_students():
     global students
     students = []
@@ -15,7 +16,7 @@ def enroll_students():
     print("Type 'done' when finished. \n")
 
     while True:
-        name = input("Enter student name: ").strip()
+        name = input("Enter student name or type 'done' to exit: ").strip()
 
         if name.lower() == "done":
             break
@@ -31,17 +32,18 @@ def enroll_students():
     print("\nEnrollment complete.")
     print("Enrolled students:", students)
 
+# Record attendance
 def record_attendance():
     global present_students, absent_students
 
     if not students:
-        print("\n No students enrolled yet.")
+        print("\nNo students enrolled yet.")
         return
     
     present_students = []
     absent_students = []
 
-    print("\n Record Attendance ")
+    print("\nRecord Attendance ")
     print("Enter the names of the Present students.")
     print("Type 'done' when finished.\n")
 
@@ -62,6 +64,56 @@ def record_attendance():
     absent_students = [s for s in students if s not in present_students]
     print("\n Attendance has been recorded.")
 
+def view_attendance():
+    while True:
+        view_input = input("""View attendance
+
+Present - Present students
+Absent - Absent students
+All - All students
+            
+Choice: """).strip()
+        
+        if view_input == "Present":
+            print("List of present students")
+            for index, present in enumerate(present_students, start=1):
+                print(f"{index} - {present}")
+            break
+
+        elif view_input == "Absent":
+            print("List of absent students")
+            for index, absent in enumerate(absent_students, start=1):
+                print(f"{index} - {absent}")
+            break
+
+        elif view_input == "All":
+            print("List of all students")
+            for index, all in enumerate(students, start=1):
+                print(f"{index} - {all}")
+            break
+        
+        else:
+            print("Choose one of the following options and try again!")
+
+def search_student_status():
+    while True:
+        search_input = input("Search student or type 'done' to exit: ")
+
+        if search_input == "done":
+            break
+
+        elif search_input not in students:
+            print("Student not in the list")
+        
+        elif search_input in present_students:
+            print(f"{search_input} marked as present")
+        
+        elif search_input in absent_students:
+            print(f"{search_input} marked as absent")
+
+        else:
+            pass
+
 def download_attendance():
     filename = "attendance_report.txt"
     with open(filename, "w") as file:
@@ -70,52 +122,58 @@ def download_attendance():
 
         file.write("Enrolled Students:\n")
         for s in students:
-            file.write(f"- {s}\n")
+            file.write(f" - {s}\n")
 
         file.write("\nPresent Students:\n")
         for p in present_students:
-            file.write(f"- {p}\n")
+            file.write(f" - {p}\n")
 
         file.write("\nAbsent Students:\n")
         for a in absent_students:
-            file.write(f"- {a}\n")
+            file.write(f" - {a}\n")
 
     print(f"\nAttendance saved to '{filename}'")
 
 # User had to choose their input
 def main():
     text_design()
-    while True:
-        print("\n Attendance Checker")
-        print("1. Enroll Students")
-        print("2. Record Attendance")
-        print("3. View Attendance Report")
-        print("4. Search Student Status")
-        print("5. Download Attendance File")
-        print("6. Exit Program")
-    
-        choice = input("Choose an option 1-6: ")
-
-        if choice == "1":
-            enroll_students()
-
-        elif choice == "2":
-            record_attendance()
-
-        elif choice == "3":
-            pass
-
-        elif choice == "4":
-            pass
-
-        elif choice == "5":
-            download_attendance()
+    try:
+        while True:
+            print("\nAttendance Checker")
+            print("1. Enroll Students")
+            print("2. Record Attendance")
+            print("3. View Attendance Report")
+            print("4. Search Student Status")
+            print("5. Download Attendance File")
+            print("6. Exit Program")
         
-        elif choice == "6":
-            break
+            choice = input("Choose an option 1-6: ")
 
-        else:
-            pass
+            if choice == "1":
+                enroll_students()
 
-main()
+            elif choice == "2":
+                record_attendance()
+
+            elif choice == "3":
+                view_attendance()
+
+            elif choice == "4":
+                search_student_status()
+
+            elif choice == "5":
+                download_attendance()
+            
+            elif choice == "6":
+                print("Exiting attendance checker")
+                break
+
+            else:
+                print("Choose among the choices and try again!")
+    
+    except KeyboardInterrupt:
+        print("\nAttendance checking cancelled")
+
+if __name__ == "__main__":
+    main()
 
